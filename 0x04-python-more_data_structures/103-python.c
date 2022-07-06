b48deb484160a72dad8e0009bcedb3fb03c6f7c2
DammyD <ibukunoluwadawodu@gmail.com>
2022-07-06 13:25:27 -0700

    CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <python.h>
+#include <Python.h>
 
 /**
- * print_python_bytes - prints bytes information
+ * print_python_bytes - Prints bytes information
+ *
  * @p: Python Object
  * Return: no return
  */
@@ -14,33 +15,34 @@ void print_python_bytes(PyObject *p)
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		printf(" [ERROR] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	size = ((PyVarObject *)(p))->ob-size;
+	size = ((PyVarObject *)(p))->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
 
-	printf(" size: %ld\n", size);
-	printf(" trying string: %s\n", string);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
 
 	if (size >= 10)
-		limit - 10;
+		limit = 10;
 	else
 		limit = size + 1;
 
-	printf(" first %ld bytes:", limit);
+	printf("  first %ld bytes:", limit);
 
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
 			printf(" %02x", string[i]);
 		else
 			printf(" %02x", 256 + string[i]);
+
 	printf("\n");
 }
 
 /**
- * print_python_list - prints list information
+ * print_python_list - Prints list information
  *
  * @p: Python Object
  * Return: no return
@@ -51,8 +53,8 @@ void print_python_list(PyObject *p)
 	PyListObject *list;
 	PyObject *obj;
 
-	size = ((pyVarObject *)(p))->ob_size;
-	list = (pyListObject *)p;
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
@@ -62,8 +64,8 @@ void print_python_list(PyObject *p)
 	{
 		obj = ((PyListObject *)p)->ob_item[i];
 		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj)0
-				print_python_bytes(obj));
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
 	}
 }
 
