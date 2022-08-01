06315a395cbe3be51afe83fd6f86d2eddb9fa59b
DammyD <ibukunoluwadawodu@gmail.com>
2022-08-01 09:43:02 -0700

    Write a class MyList that inherits from list

--- 0x0A-python-inheritance/tests/1-my_list.py
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -6,7 +6,7 @@ This program creates a class called MyList that inherits of the class List
 
 class MyList(list):
     """
-    This class inherits from the class list and can print it's elements sorted
+    This class inherits form the class list an can print it's elements sorted
     """
 
     def print_sorted(self):
