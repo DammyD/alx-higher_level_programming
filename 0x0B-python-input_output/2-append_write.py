7792fe0d50154aeeac423ca69fba8886a05c73df
DammyD <ibukunoluwadawodu@gmail.com>
2022-08-02 13:32:03 -0700

    Append a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A script that appends a string at the end of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    returns the number of character
+    """
+    count = 0
+    with open(filename, 'a', encoding="UTF-8") as f:
+        for c in text:
+            f.write(c)
+            count += 1
+
+            return (count)
