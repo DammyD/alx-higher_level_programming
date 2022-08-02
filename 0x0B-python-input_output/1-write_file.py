428699dfe465d70117bf534f464e332a2bf2896b
DammyD <ibukunoluwadawodu@gmail.com>
2022-08-02 13:16:48 -0700

    write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-A script that writes a file and returns the number of characters
+A script that writes to a file and returns the number of characters
 """
 
 def write_file(filename="", text=""):
-    """ Function returns the no of character written to filename"""
+    """Function returns the no of character written to filname"""
     count = 0
     with open(filename, 'w', encoding="UTF+8") as filen:
         for c in text:
             filen.write(c)
             count += 1
 
-            return (count)
+    return (count)
