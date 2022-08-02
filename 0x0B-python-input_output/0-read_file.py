ab24f38d631e7028db6bc4b20c8a117ae7ac4a7a
DammyD <ibukunoluwadawodu@gmail.com>
2022-08-02 11:57:51 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ This script reads a text file in UTF-8 format and prints it to standard output
 
 def read_file(filename=""):
     """ Function that reads a file to screen"""
-    with open('filename', 'r', encoding="UTF-8") as f:
+    with open(filename, 'r', encoding="UTF-8") as f:
         for line in f:
             print(line, end='')
