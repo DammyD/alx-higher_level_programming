663c854ed6312950ef275da4185e823d18f7835f
DammyD <ibukunoluwadawodu@gmail.com>
2022-10-08 15:30:14 -0700

    Task 5 - updated

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     db = connection.cursor()
     db.execute("SELECT cities.name FROM cities" +
                " INNER JOIN states ON cities.state_id = states.id" +
-               "WHERE states.name LIKE %s;", [argv[4]])
+               " WHERE states.name LIKE %s;", [argv[4]])
     query_rows = db.fetchall()
     print(', '.join(map(lambda x: x[0], query_rows)))
 
